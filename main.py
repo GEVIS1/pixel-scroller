6f5a76c199d61f7fb0bd5610e9df35641a89b8e5
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2021-04-02 14:38:53 +1300

    Added functionality to draw previous LEDs

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 xy = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 # Set default LED position to be the middle
-count = 12
+count = 0
 
 
 def on_button_pressed_a():
     global count
     
-    # Check if count is 0 and if it is wrap around to 24
-    if (count == 0):
-        count = 24
-        pass
+    basic.clear_screen()
+
+    # Check if count is -1 and if it is wrap around to 25
+    if (count == -1):
+        count = 25
     else:
-        --count   
-        pass
-    pass
+        --count
 
 def on_button_pressed_b():
     global count  
     
-    # Check if count is 24 and if it is wrap around to 0
-    if (count == 24):
+    basic.clear_screen()
+
+    # Check if count is 25 and if it is wrap around to 0
+    if (count == 25):
         count = 0
-        pass 
     else:
         ++count
-        pass
 pass
 
 def main():
     global count
-    basic.clear_screen()
 
     input.on_button_pressed(Button.A, on_button_pressed_a)
     input.on_button_pressed(Button.B, on_button_pressed_b)
@@ -38,10 +36,9 @@ def main():
     # and turn it on if the count is up to that LED from origin 0,0 in the top left corner.
     for y in range(5):
         for x in range(5):
-            if (count == x + (y * 5)):
-                led.plot(x, y)
-                pass
-            pass
-        pass
-    pass
+            for i in range(count):
+                led.plot(i % 5, i / 5)
+            
+        
+    
 basic.forever(main)
--- main.ts
+++ main.ts
@@ -1,32 +1,28 @@
 let xy = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 //  Set default LED position to be the middle
-let count = 12
+let count = 0
 
 basic.forever(function main() {
     
-    basic.clearScreen()
     input.onButtonPressed(Button.A, function on_button_pressed_a() {
         
-        //  Check if count is 0 and if it is wrap around to 24
-        if (count == 0) {
-            count = 24
-            
+        basic.clearScreen()
+        //  Check if count is -1 and if it is wrap around to 25
+        if (count == -1) {
+            count = 25
         } else {
             --count
-            
         }
-        
         
     })
     input.onButtonPressed(Button.B, function on_button_pressed_b() {
         
-        //  Check if count is 24 and if it is wrap around to 0
-        if (count == 24) {
+        basic.clearScreen()
+        //  Check if count is 25 and if it is wrap around to 0
+        if (count == 25) {
             count = 0
-            
         } else {
             ++count
-            
         }
         
     })
@@ -34,14 +30,9 @@ basic.forever(function main() {
     //  and turn it on if the count is up to that LED from origin 0,0 in the top left corner.
     for (let y = 0; y < 5; y++) {
         for (let x = 0; x < 5; x++) {
-            if (count == x + y * 5) {
-                led.plot(x, y)
-                
+            for (let i = 0; i < count; i++) {
+                led.plot(i % 5, i / 5)
             }
-            
-            
         }
-        
     }
-    
 })
